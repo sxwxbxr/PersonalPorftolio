e6f2ffa31225700a5e8bfb77b132039531089eb5
ApaX <swbr@proton.me>
2024-01-17 16:27:20 +0100

    Created  intial look of the homepage
    Added a navigation bar to on top of the website.
    Created function to jump smoothly to different <div> items.
    Designed a basic styling

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Function to scroll to a specific section
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const welcomeScreen = document.querySelector(".welcome-screen");
+
+    welcomeScreen.addEventListener("mousemove", function (e) {
+        const { clientX: x, clientY: y } = e;
+        const percentX = (x / window.innerWidth) * 100;
+        const percentY = (y / window.innerHeight) * 100;
+
+        welcomeScreen.style.background = `radial-gradient(circle at ${percentX}% ${percentY}%, rgba(255, 255, 255, 0.2), transparent)`;
+    });
+});
